57cb76ee55709938520110c24fb05a84002ee976
Zain Ul Hassan <101182681+zainjan56@users.noreply.github.com>
2023-12-09 22:39:19 +0500

    Update Home.js

--- my-fyp/src/Learningapp/components/Home.js
+++ my-fyp/src/Learningapp/components/Home.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaLaptop } from 'react-icons/fa';
-import { GiWorld } from 'react-icons/gi';
-import { GiSkills } from 'react-icons/gi';
-import { GiSpellBook } from 'react-icons/gi';
 import Carousel from 'react-bootstrap/Carousel';
 import "./Home.css";
 import picture from "./images/carousel1.png";
@@ -12,6 +8,20 @@ import pictureThree from "./images/carousel3.jpg";
 import homeIcon from "./images/learning2.png";
 import Header from './Header';
 import Footer from "./Footer";
+import courseOne from '../components/images/course1.jpg'
+import courseTwo from '../components/images/course2.jpg'
+import courseThree from '../components/images/course3.jpg'
+import courseFour from '../components/images/course4.jpg'
+
+import {
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCol
+} from 'mdb-react-ui-kit';
 
 const Home = () => {
  
@@ -62,38 +72,69 @@ const Home = () => {
       </div>
 
       <div className='row'>
-        <div className='col-sm-3 missionCol'>
-          <div className='mission'>
-            <div className='missionOnline'><i><FaLaptop size={30} color='green'/></i>
-            <h6>Free Of Cost Courses</h6>
-            <p>Our all courses are available free of cost and anyone can enroll themselves and to polish their skills.</p>
-            </div>
-          </div>
-        </div>
-        <div className='col-sm-3 missionCol'>
-        <div className='mission'>
-        <div className='missionOnline'><i><GiWorld size={30} color='green'/></i>
-            <h6>Worldwide Accessible</h6>
-            <p>Anyone from any part of the world can access all our courses.</p>
-            </div>
-        </div>
-        </div>
-        <div className='col-sm-3 missionCol'>
-        <div className='mission'>
-        <div className='missionOnline'><i><GiSkills size={30} color='green'/></i>
-            <h6>Skills Assessment</h6>
-            <p>Anyone can take quizzes in different courses to assess and improve their skills on our Online Education Platform.</p>
-            </div>
-        </div>
-        </div>
-        <div className='col-sm-3 missionCol'>
-        <div className='mission'>
-        <div className='missionOnline'><i><GiSpellBook size={30} color='green'/></i>
-            <h6>Exams</h6>
-            <p>By completing different courses, students will be able to pass different job exams and interviews.</p>
-            </div>
-        </div>
-        </div>
+      <MDBRow className='row-cols-1 row-cols-md-4 g-4 h-200'>
+      <MDBCol>
+        <MDBCard>
+          <MDBCardImage
+            src={courseOne}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Free Of Cost Courses</MDBCardTitle>
+            <MDBCardText>
+            Our all courses are available free of cost and anyone can enroll themselves and to polish their skills.
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard>
+          <MDBCardImage
+            src={courseTwo}
+            alt='...'
+            position='top'
+            className='cardFeature'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Worldwide Accessible</MDBCardTitle>
+            <MDBCardText>
+            Anyone from any part of the world can access all our courses without any charges and start their learning journey.
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard>
+          <MDBCardImage
+            src={courseThree}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Skills Assessment</MDBCardTitle>
+            <MDBCardText>
+            Anyone can take quizzes in different courses to assess and improve their skills on our Online Education Platform.
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard>
+          <MDBCardImage
+            src={courseFour}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Exams</MDBCardTitle>
+            <MDBCardText>
+            By completing different courses, students will be able to pass different job exams and interviews.
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
       </div>
     </div>
     <Footer />
